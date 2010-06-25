630319a9757e228af75c173486b38adf81ff886a
Bernardo Botelho (botelho.bernardo@gmail.com) <Bernardo Botelho (botelho.bernardo@gmail.com)>
2010-06-25 09:20:53 -0300

    Adding BoudingBox.

---
+++ Mood/BoundingBox.cs
@@ -0,0 +1,18 @@
+﻿namespace Mood
+{
+    public class BoundingBox
+    {
+        public Vector3d A { get; set; }
+        public Vector3d B { get; set; }
+        public Vector3d C { get; set; }
+        public Vector3d D { get; set; }
+
+        public BoundingBox(Vector3d a, Vector3d b, Vector3d c, Vector3d d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+    }
+}
